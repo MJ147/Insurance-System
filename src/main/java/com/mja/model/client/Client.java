b59653764f61bf6f20039996d665d751f55695ae
Marek Jaczewski <jaczewski147@gmail.com>
2019-12-08 21:19:46 +0100

    add fields

---
+++ src/main/java/com/mja/model/client/Client.java
@@ -0,0 +1,11 @@
+package com.mja.model.client;
+
+import com.mja.model.policy.InsurancePolicy;
+
+public class Client {
+
+    private Long id;
+    private String firstName;
+    private String secondName;
+    private InsurancePolicy insurancePolicy;
+}
