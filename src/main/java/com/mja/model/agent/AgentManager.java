79381b655bba3dab449cb0b39b2c3d205a34ce8c
Marek Jaczewski <jaczewski147@gmail.com>
2019-12-08 21:13:34 +0100

    add fields

---
+++ src/main/java/com/mja/model/agent/AgentManager.java
@@ -0,0 +1,6 @@
+package com.mja.model.agent;
+
+public class AgentManager extends InsuranceAgent {
+
+    private InsuranceAgent insuranceAgent;
+}
---
+++ src/main/java/com/mja/model/agent/InsuranceAgent.java
@@ -0,0 +1,11 @@
+package com.mja.model.agent;
+
+import com.mja.model.policy.InsurancePolicy;
+
+public class InsuranceAgent {
+
+    private Long id;
+    private String firstName;
+    private String secondName;
+    private InsurancePolicy insurancePolicy;
+}
