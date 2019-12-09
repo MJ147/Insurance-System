e013ddcf065d4e3282a3e24ab2768761fc121e0d
Marek Jaczewski <jaczewski147@gmail.com>
2019-12-09 21:26:15 +0100

    add lombok, add annotations

--- src/main/java/com/mja/model/agent/AgentManager.java
+++ src/main/java/com/mja/model/agent/AgentManager.java
@@ -1,5 +1,11 @@
 package com.mja.model.agent;
 
+import lombok.Data;
+
+import javax.persistence.Entity;
+
+@Entity
+@Data
 public class AgentManager extends InsuranceAgent {
 
     private InsuranceAgent insuranceAgent;
--- src/main/java/com/mja/model/client/Client.java
+++ src/main/java/com/mja/model/client/Client.java
@@ -1,9 +1,19 @@
 package com.mja.model.client;
 
 import com.mja.model.policy.InsurancePolicy;
+import lombok.Data;
 
+import org.springframework.data.annotation.Id;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+
+@Entity
+@Data
 public class Client {
 
+    @Id
+    @GeneratedValue
     private Long id;
     private String firstName;
     private String secondName;
