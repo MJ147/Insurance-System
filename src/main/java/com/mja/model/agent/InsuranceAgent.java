3f00ac5539ea8745b11273db9d060d91af7f873d
Marek Jaczewski <jaczewski147@gmail.com>
2020-02-07 17:25:39 +0100

    Remove inheritance, add constructors.

--- src/main/java/com/mja/model/agent/InsuranceAgent.java
+++ src/main/java/com/mja/model/agent/InsuranceAgent.java
@@ -1,14 +1,28 @@
 package com.mja.model.agent;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import lombok.Data;
+import lombok.RequiredArgsConstructor;
 
 import javax.persistence.*;
 
 @Entity
 @Data
-public class InsuranceAgent extends Agent {
+@RequiredArgsConstructor
+public class InsuranceAgent  {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String secondName;
     @ManyToOne
     @JoinColumn(name = "agent_manager_id")
+    @JsonBackReference
     private AgentManager agentManager;
+
+    public InsuranceAgent(String firstName, String secondName) {
+        this.firstName = firstName;
+        this.secondName = secondName;
+    }
 }
