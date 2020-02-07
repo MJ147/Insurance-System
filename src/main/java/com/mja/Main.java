0f971b58fcca8c7a1334c96454df1bab5436d4e8
Marek Jaczewski <jaczewski147@gmail.com>
2020-02-07 23:07:21 +0100

    Create launch method of application

---
+++ src/main/java/com/mja/Main.java
@@ -0,0 +1,13 @@
+package com.mja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+@SpringBootApplication
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+    }
+}
