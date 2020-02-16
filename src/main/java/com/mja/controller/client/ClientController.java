22982cf501b87787fed35c979c49320f482cf4cd
Marek Jaczewski <jaczewski147@gmail.com>
2020-02-16 22:54:11 +0100

    Create POST request save client

---
+++ src/main/java/com/mja/controller/client/ClientController.java
@@ -0,0 +1,25 @@
+package com.mja.controller.client;
+
+import com.mja.model.client.Client;
+import com.mja.service.client.ClientService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/client")
+@RestController
+public class ClientController {
+
+    private ClientService clientService;
+
+    public ClientController(ClientService clientService) {
+        this.clientService = clientService;
+    }
+
+    @PostMapping("/add_client")
+    public ResponseEntity<Client> addClient(@RequestBody Client client) {
+        return ResponseEntity.ok(clientService.save(client));
+    }
+}
